d9da95ac4990b85feaa164ab213daed8b4a7df0a
Larissa <larissarafaely2015@gmail.com>
2023-04-17 12:52:26 -0300

    modificações na página contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,43 +1,101 @@
 import React, {useState} from "react";
 import "./style-contact.css"
 
-function Contact(props) {
+function Contact() {
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [telefone, setTelefone] = useState("");
+    const [mensage, setMensage] = useState("");
+
+    function enviarFormulario(event) {
+        event.preventDefault();
+
+        console.log("Enviado com Sucesso!");
+        
+        const texto = `Nome: ${nome}\nE-mail: ${email}\nTelefone: ${telefone}\nMensage: ${mensage}`;
+        const numeroDoWhatsApp = import.meta.env.VITE_WHATSAPP_NUMBER;
+        const linkWhatsApp = `https://wa.me/${numeroDoWhatsApp}?text=${encodeURIComponent(texto)}`;
+        window.open(linkWhatsApp, '_blank');
+    }
+
+    function mascaraTelefone(event) {
+        const texto = event.target.value;
+        const textoApenasNumero = texto.replace(/\D/g, '').substring(0, 11);
+            let telefoneFormatado = textoApenasNumero.replace(/^(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+                if (textoApenasNumero.length < 11) {
+                    telefoneFormatado = textoApenasNumero.replace(/^(\d{2})(\d{4})(\d{0,4})/, '($1) $2-$3');
+                }
+                setTelefone(telefoneFormatado);
+    }
+
     return (
         <main>
-        <form id="form">
-            <div class="form-control">
-                <label for="username">Nome e Sobrenome</label>
-                <input type="text" id="username" placeholder="Digite seu nome e sobrenome."/>
-                <i class="fas fa-exclamation-circle"></i>
-                <i class="fas fa-check-circle"></i>
-                <small>Mensagem de Erro</small>
-            </div>
 
-            <div class="form-control ">
-                <label for="email">Email</label>
-                <input type="text" id="email" placeholder="Digite seu e-mail."/>
-                <i class="fas fa-exclamation-circle"></i>
-                <i class="fas fa-check-circle"></i>
-                <small>Mensagem de Erro</small>
-            </div>
+            <form onSubmit={enviarFormulario}>
 
-            <div class="form-control"> 
-                <label for="telefone">Telefone</label>
-                <input type="text" id="telefone" placeholder="(99) 99999-9999" maxlength="15" />
-                <i class="fas fa-exclamation-circle"></i>
-                <i class="fas fa-check-circle"></i>
-                <small>Mensagem de Erro</small>
-            </div>
+                <fieldset>
+                    <div class="form-control">
+                    <label for="username">Nome e Sobrenome</label>
+                    <input 
+                        type="text" 
+                        id="username" 
+                        required
+                        minLength="2"
+                        placeholder="Digite seu nome e sobrenome."
+                        value={nome}
+                        onChange={(event) => setNome(event.target.value)}
+                    />
+                    </div>
+                </fieldset>
 
-            <div>
-                    <label for="mensage">Mensagem</label>
-                    <textarea name="mensage" id="mensage" cols="60" rows="9" class="input-padrao"></textarea>
-            </div>
+                <fieldset>
+                    <div class="form-control ">
+                        <label for="email">Email</label>
+                        <input 
+                        type="text"
+                        id="email" 
+                        required
+                        placeholder="Digite seu e-mail."
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                        />
+                    </div>
+                </fieldset>
 
-                    <button id="button" onclick="event.preventDefault()" type="submit">Enviar</button>                
-                </form>
-            </main>
+                <fieldset>
+                    <div class="form-control"> 
+                        <label for="telefone">Telefone</label>
+                        <input 
+                        type="text" 
+                        id="telefone" 
+                        required
+                        placeholder="(99) 99999-9999" 
+                        pattern="^\(\d{2}\) \d{5}-\d{4}$"
+                        maxLength="15"
+                        value={telefone}
+                        onChange={mascaraTelefone} 
+                        />
+                    </div>
+                </fieldset>
 
+                <fieldset>
+                    <div>
+                        <label for="mensage">Mensagem</label>
+                        <textarea 
+                        name="mensage" 
+                        id="mensage" 
+                        cols="60" 
+                        rows="9" 
+                        class="input-padrao"
+                        value={mensage}
+                        onChange={(event) => setMensage(event.target.value)}
+                        />
+                    </div>
+                </fieldset>
+
+                    <button id="button" type="submit">Enviar</button>                
+            </form>
+        </main>
     )
 }
 
